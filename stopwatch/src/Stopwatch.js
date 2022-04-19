d57d63da80325c6fc65031a5198ac937dd7ecca8
Anne-Hyeyeon <khy2106@naver.com>
2022-04-19 12:16:15 +0900

    0419

--- stopwatch/src/Stopwatch.js
+++ stopwatch/src/Stopwatch.js
@@ -11,7 +11,7 @@ class Stopwatch extends Component {
     this.timerID = 0
   }
   componentWillMount(){
-    this.timerId = setInterval(e=>{this.tick()},1000)
+    this.timerId = setInterval(e=>{this.tick()},100)
   }
 
   componentWillUnmount(){
@@ -26,7 +26,19 @@ class Stopwatch extends Component {
     }
   }
 
+
+
   getDisp(){
+    const z2 = (v)=>{
+      const l = '00' + v
+      return l.substr(l.length - 2,2)
+    }
+
+    const z3 = (v)=>{
+      const l = '0' + v
+      return l.substr(l.length - 4,2)
+    }
+
     const s = this.state
     const delta = s.curTime - s.startTime
     const t = Math.floor(delta/1000)
@@ -34,7 +46,11 @@ class Stopwatch extends Component {
     const m = Math.floor(t / 60)
     const mm = m % 60
     const hh = Math.floor(mm/60)
-    return <span className='disp'>{hh}:{mm}:{ss}</span>
+    
+    // const z3 = delta.substr(delta.length - 2,2)
+
+
+    return <span className='disp'>{z2(hh)}:{z2(mm)}:{z2(ss)}:{z3(delta)}</span>
   }
   render() {
     let label = 'START'
